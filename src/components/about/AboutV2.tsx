1790f5009d59352662dc4a560d1dc023f09261b4
oyukaaus <oyukaau@gmail.com>
2025-04-28 23:34:06 +0800

    last update

--- src/components/about/AboutV2.tsx
+++ src/components/about/AboutV2.tsx
@@ -1,37 +1,42 @@
-"use client"
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import thumb2 from "@/assets/img/thumb/2.jpg"
-import thumb5 from "@/assets/img/thumb/5.jpg"
-import ModalVideo from 'react-modal-video';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import thumb2 from "@/assets/img/thumb/2.jpg";
+import thumb5 from "@/assets/img/thumb/5.jpg";
+import ModalVideo from "react-modal-video";
 
 const AboutV2 = () => {
+  const [isOpen, setOpen] = useState(false);
 
-    const [isOpen, setOpen] = useState(false);
-
-    return (
-        <>
-            <div className="about-style-two-area default-padding">
-                <div className="container">
-                    <div className="row align-center">
-                        <div className="col-xl-6 offset-xl-1 col-lg-6 order-lg-last">
-                            <div className="about-style-two-thumb">
-                                <div className="thumb">
-                                    <Image src={thumb5} alt="Image Not Found" />
-                                    <div className="shape-card text-light" style={{ backgroundImage: 'url(/assets/img/shape/21.png)' }}>
-                                        <h4>Санхүүгийн хяналтыг таны гарт</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-5 col-lg-6">
-                            <h4 className="sub-title">Бидний тухай</h4>
-                            <h2 className="title">Бид таны татвар, тайлангийн асуудлыг шийднэ.</h2>
-                            <p>
-                            Digitax бол таны бизнесийн үйл ажиллагааны өдөр тутмын борлуулалт, зардал, төлбөр тооцоог бүртгэн санхүү, татвар, НДШ, НӨАТ-ын тайланг цаг тухай бүрт дижитал системийн тусламжтай мэргэжлийн нягтлан бодогч нар бэлтгэн илгээж таны цаг завыг хэмнэх гэрээт цахим үйлчилгээ юм.
-                            </p>
-                            <div className="card-style-two mt-40">
+  return (
+    <>
+      <div className="about-style-two-area default-padding">
+        <div className="container">
+          <div className="row align-center">
+            <div className="col-xl-6 offset-xl-1 col-lg-6 order-lg-last">
+              <div className="about-style-two-thumb">
+                <div className="thumb">
+                  <Image src={thumb5} alt="Image Not Found" />
+                  <div
+                    className="shape-card text-light"
+                    style={{ backgroundImage: "url(/assets/img/shape/21.png)" }}
+                  >
+                    <h4>DiGiTAX таны санхүүгийн хөтөч — хэзээ ч, хаана ч</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-5 col-lg-6">
+              <h4 className="sub-title">Бидний тухай</h4>
+              <h2 className="title">Дижитакс төсөл</h2>
+              <p>
+                ДИЖИТАКС ТӨСӨЛ нь санхүү, татвар, технологийн туршлагатай,
+                чадварлаг мэргэжилтнүүдийн хэрэгжүүлж буй цахим тайлагналын
+                систем бөгөөд бичил, жижиг, хувиараа бизнес эрхлэгчдэд
+                зориулагдсан.
+              </p>
+              {/* <div className="card-style-two mt-40">
                                 <div className="thumb">
                                     <Image src={thumb2} alt="Image Not Found" />
                                     <ModalVideo channel='youtube' isOpen={isOpen} videoId="aTC_RNYtEb0" onClose={() => setOpen(false)} />
@@ -41,13 +46,13 @@ const AboutV2 = () => {
                                     <h2>3.8 X</h2>
                                     <h5>Хялбар тайлагнал </h5>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                            </div> */}
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutV2;
--- src/components/footer/FooterV1.tsx
+++ src/components/footer/FooterV1.tsx
@@ -1,82 +1,101 @@
-import Link from 'next/link';
-import FooterNewsLetter from '../form/FooterNewsLetter';
+import Link from "next/link";
+import FooterNewsLetter from "../form/FooterNewsLetter";
 
 const FooterV1 = () => {
-    return (
-        <>
-            <footer className="bg-dark overflow-hidden text-light">
-                <div className="container">
-                    <div className="f-items default-padding">
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6 footer-item pr-30 pr-md-15 pr-xs-15">
-                                <div className="f-item address">
-                                    <ul className="contact-address">
-                                        <li>
-                                            <p>Бидэнтэй холбогдох</p>
-                                            <h4><a href="mailto:info@validthemes.com">taxtime8899@gmail.com</a></h4>
-                                        </li>
-                                        <li>
-                                            <p>Холбоо барих</p>
-                                            <h4><a href="tel:+4733378901">Утас:99015440, 99097756, 99160604</a></h4>
-                                        </li>
-                                        <li>
-                                            <p>Манай хаяг</p>
-                                            <h4>Сүхбаатар дүүрэг 6 дугаар хороо, Монреимпекс ХХК-ийн байр, 208 тоот</h4>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 footer-item">
-                                <div className="f-item link">
-                                    <h4 className="widget-title">Холбоос</h4>
-                                    <ul>
-                                        <li>
-                                            <Link href="/about-us">Компаний танилцуулга</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/contact-us">Тусламж</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/pricing">Үйлчилгээ</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                           
-                            <div className="col-lg-4 col-md-6 footer-item">
-                                <div className="f-item newsletter">
-                                    <p>
-                                        Имэйлээр мэдээ, мэдээлэл <br /> цаг хугацаанд нь авах
-                                    </p>
-                                    <FooterNewsLetter />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="footer-bottom">
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <p>&copy; Copyright {(new Date().getFullYear())}. All Rights Reserved by <a href="https://digitax.mn" target='_blank'>Digitax</a></p>
-                            </div>
-                            <div className="col-lg-6 text-end">
-                                <ul className="link-list">
-                                    <li>
-                                        <Link href="#" scroll={false}>Үйлчилгээний нөхцөл</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="#" scroll={false}>Нууцлалын бодлого</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/contact-us">Тусламж</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <footer className="bg-dark overflow-hidden text-light">
+        <div className="container">
+          <div className="f-items default-padding">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 footer-item pr-30 pr-md-15 pr-xs-15">
+                <div className="f-item address">
+                  <ul className="contact-address">
+                    <li>
+                      <p>Бидэнтэй холбогдох</p>
+                      <h4>
+                        <a href="mailto:info@validthemes.com">
+                          taxtime8899@gmail.com
+                        </a>
+                      </h4>
+                    </li>
+                    <li>
+                      <p>Холбоо барих</p>
+                      <h4>
+                        <a href="tel:+4733378901">Утас: 77114555</a>
+                      </h4>
+                    </li>
+                    <li>
+                      <p>Манай хаяг</p>
+                      <h4>
+                        Чингэлтэй дүүрэг, 1-р хороо, Бага тойруу 17 Жи Жи Тауэр,
+                        2 давхар, 202 тоот
+                      </h4>
+                    </li>
+                  </ul>
                 </div>
-            </footer>
-        </>
-    );
+              </div>
+              <div className="col-lg-4 col-md-6 footer-item">
+                <div className="f-item link">
+                  <h4 className="widget-title">Холбоос</h4>
+                  <ul>
+                    <li>
+                      <Link href="/about-us">Бидний тухай</Link>
+                    </li>
+                    <li>
+                      <Link href="/contact-us">Тусламж</Link>
+                    </li>
+                    <li>
+                      <Link href="/pricing">Үйлчилгээ</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6 footer-item">
+                <div className="f-item newsletter">
+                  <p>
+                    Имэйлээр мэдээ, мэдээлэл <br /> цаг хугацаанд нь авах
+                  </p>
+                  <FooterNewsLetter />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <div className="row">
+              <div className="col-lg-6">
+                <p>
+                  &copy; Copyright {new Date().getFullYear()}. All Rights
+                  Reserved by{" "}
+                  <a href="https://digitax.mn" target="_blank">
+                    Digitax
+                  </a>
+                </p>
+              </div>
+              <div className="col-lg-6 text-end">
+                <ul className="link-list">
+                  <li>
+                    <Link href="#" scroll={false}>
+                      Үйлчилгээний нөхцөл
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#" scroll={false}>
+                      Нууцлалын бодлого
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/contact-us">Тусламж</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default FooterV1;
